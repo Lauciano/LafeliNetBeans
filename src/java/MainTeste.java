88baca30cfae3626d2d03f60ee8eec358edddf82
Lauciano <jose.lauciano@gmail.com>
2014-08-21 09:07:02 -0300

    Persistência Rodou

--- src/java/MainTeste.java
+++ src/java/MainTeste.java
@@ -2,16 +2,16 @@ import persistencia.Cliente;
 import persistencia.ClienteDAO;
 
 public class MainTeste {
-	public static void main(String[] args){
-		
-		Cliente c = new Cliente();
-		c.setNome("Lauciano");
-		c.setSobrenome("Almeida");
-		c.setTelefonep("12345678");
-		
-		ClienteDAO cdao = new ClienteDAO();
-		
-		cdao.persist(c);
-		
-	}
+    public static void main(String[] args){
+
+        Cliente c = new Cliente();
+        c.setNome("Lauciano");
+        c.setSobrenome("Almeida");
+        c.setTelefonep("12345678");
+
+        ClienteDAO cdao = new ClienteDAO();
+
+        cdao.persist(c);
+
+    }
 }
