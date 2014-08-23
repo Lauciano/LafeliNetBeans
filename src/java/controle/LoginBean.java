eed1ed55e35fc71d765b96932556710815f4d2e0
Lauciano <jose.lauciano@gmail.com>
2014-08-23 16:54:43 -0300

    Index e Busca

--- src/java/controle/LoginBean.java
+++ src/java/controle/LoginBean.java
@@ -43,7 +43,7 @@ public class LoginBean implements Serializable {
         username = "Nome de Usuário";
         password = "Senha";
         isLoggedIn = false;
-        return "faces/index";
+        return "index.xhtml";
     }
     
     /**
